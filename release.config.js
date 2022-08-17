5969d65617f0fb82b950eeebcbf24fa1514103b6
Jean Baptiste Renault <renault.jeanbaptiste@gmail.com>
2022-08-17 10:39:45 +0200

    chore: init nx for monorepo

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'conventionalcommits',
+  presetConfig: {
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'chore', section: 'Chores' },
+      { type: 'docs', hidden: true },
+      { type: 'style', hidden: true },
+      { type: 'refactor', section: 'Refactoring' },
+      { type: 'perf', hidden: true },
+      { type: 'test', hidden: true },
+    ],
+  },
+  branches: ['main', { name: 'beta', prerelease: true }],
+  releaseRules: [{ type: 'refactor', release: 'patch' }],
+};
